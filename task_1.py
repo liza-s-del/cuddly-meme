a06b058fb043e9432186cb1b53444bfe0c1236ea
liza-s-del <lizasigareva21@gmail.com>
2025-12-10 23:30:39 +0300

    Лабораторная работа №1

---
+++ task_1.py
@@ -0,0 +1,7 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+numbers_without_none = numbers[0:4] + numbers[5:]
+average = sum(numbers_without_none) / len(numbers)
+numbers[4] = average
+print("Измененный список:", numbers)
